7d5285bb826fa44f40033116ad93564d288e7b50
prosenjitkumar <prosenjitearnkuar@gmail.com>
2019-04-21 16:44:39 +0600

    new something

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # projects
+Python --------- Computer Application, Web Application, Django/Flask, PyQt5, Pandas/Numpy/Tensorflow,
+ Databases(PostGreSQL, MySQL, SQLite) ETC.
---
+++ databases/mysql/func.sql
@@ -0,0 +1,21 @@
+show databases
+
+CREATE DATABASE pyqt5;
+
+show databases;
+
+CREATE TABLE employee (
+    id   INT,
+    name VARCHAR(35),
+    email text
+)
+
+CREATE TABLE employee1 (
+    name VARCHAR(35),
+    email text
+)
+
+
+show tables;
+
+select * from employee1
